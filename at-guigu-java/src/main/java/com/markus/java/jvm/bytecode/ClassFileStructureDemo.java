ca8a8257e97cb2a12bc45c1700c959156a2eaea3
markuszcl99 <e_markuszhang@163.com>
2022-09-25 22:32:02 +0800

    字节码指令相关的代码

---
+++ at-guigu-java/src/main/java/com/markus/java/jvm/bytecode/ClassFileStructureDemo.java
@@ -0,0 +1,18 @@
+package com.markus.java.jvm.bytecode;
+
+/**
+ * @author: markus
+ * @date: 2022/9/24 11:20 PM
+ * @Description: 类文件细节
+ * @Blog: http://markuszhang.com
+ * It's my honor to share what I've learned with you!
+ */
+public class ClassFileStructureDemo {
+    private int num = 1;
+
+    public int add(){
+        num = num + 2;
+        return num;
+
+    }
+}
